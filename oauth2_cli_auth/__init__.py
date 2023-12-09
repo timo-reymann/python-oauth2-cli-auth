b1d816c3085e15d433a2239c0545cdf910147fa5
Timo Reymann <mail@timo-reymann.de>
2023-12-09 14:41:51 +0100

    docs: Add quality badges

--- oauth2_cli_auth/__init__.py
+++ oauth2_cli_auth/__init__.py
@@ -1,3 +1,7 @@
+"""
+Authenticate against OAuth2 Provider in Python CLIs
+"""
+
 from oauth2_cli_auth.http_server import OAuthCallbackHttpServer
 from oauth2_cli_auth.code_grant import OAuth2ClientInfo, exchange_code_for_access_token, get_auth_url, open_browser
 from oauth2_cli_auth.simplified_flow import get_access_token_with_browser_open
