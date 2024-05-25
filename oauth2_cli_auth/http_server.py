3ad2019cfdc82052e532b66d862b0e5f55cb3c3c
Timo Reymann <timo.reymann@deepl.com>
2024-05-25 21:45:08 +0200

    refactor: Cleanup OAuthCallbackHttpServer

--- oauth2_cli_auth/http_server.py
+++ oauth2_cli_auth/http_server.py
@@ -5,7 +5,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from string import Template
 from typing import Optional
 from urllib.parse import parse_qs, urlparse
-from oauth2_cli_auth._timeout import _method_with_timeout, TimeoutException
+
+from oauth2_cli_auth._timeout import TimeoutException, _method_with_timeout
 
 
 class CallbackPageTemplate:
@@ -32,118 +33,120 @@ class CallbackPageTemplate:
     </svg>
     """
 
-    PAGE_TEMPLATE = Template("""
-    <html lang="$lang">
-        <head>
-            <title>$title</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta name="charset" content="utf-8">
-            <style>
-                * {
-                    margin: 0;
-                    padding: 0;
-                }
-
-                body {
-                    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-                }
-
-                @media (prefers-color-scheme: dark) {
-                    body {
-                        background: rgb(34, 39, 46);
-                        color: rgb(173, 186, 199);
-                    }
-                }
-
-                html, body {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    height: 100%;
-                }
-
-                h1 {
-                    font-size: 4rem;
-                }
-
-                p {
-                    font-size: 1.4rem;
-                    max-width: 70ch;
-                }
-
-                .message {
-                    text-align: center;
-                }
-
-                .animation-ctn {
-                    text-align: center;
-                }
-
-                @keyframes checkmark {
-                    0% {
-                        stroke-dashoffset: 100px
-                    }
-
-                    100% {
-                        stroke-dashoffset: 0px
-                    }
-                }
-
-                @keyframes checkmark-circle {
-                    0% {
-                        stroke-dashoffset: 480px
-                    }
-
-                    100% {
-                        stroke-dashoffset: 960px
-                    }
-                }
-
-                @keyframes colored-circle {
-                    0% {
-                        opacity: 0
-                    }
-
-                    100% {
-                        opacity: 100
-                    }
-                }
-
-                .icon svg {
-                    padding: 1rem;
-                }
-
-                .icon svg polyline {
-                    -webkit-animation: checkmark 0.25s ease-in-out 0.7s backwards;
-                    animation: checkmark 0.25s ease-in-out 0.7s backwards
-                }
-
-                .icon svg circle {
-                    -webkit-animation: checkmark-circle 0.6s ease-in-out backwards;
-                    animation: checkmark-circle 0.6s ease-in-out backwards;
-                }
-
-                .icon svg circle#colored {
-                    -webkit-animation: colored-circle 0.6s ease-in-out 0.7s backwards;
-                    animation: colored-circle 0.6s ease-in-out 0.7s backwards;
-                }
-            </style>
-        </head>
-
-        <body>
-        <div class="message">
-            <div class="animation-ctn">
-                <div class="icon">
-                    $svg
+    PAGE_TEMPLATE = Template(
+        """
+            <html lang="$lang">
+                <head>
+                    <title>$title</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta name="charset" content="utf-8">
+                    <style>
+                        * {
+                            margin: 0;
+                            padding: 0;
+                        }
+        
+                        body {
+                            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+                        }
+        
+                        @media (prefers-color-scheme: dark) {
+                            body {
+                                background: rgb(34, 39, 46);
+                                color: rgb(173, 186, 199);
+                            }
+                        }
+        
+                        html, body {
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            height: 100%;
+                        }
+        
+                        h1 {
+                            font-size: 4rem;
+                        }
+        
+                        p {
+                            font-size: 1.4rem;
+                            max-width: 70ch;
+                        }
+        
+                        .message {
+                            text-align: center;
+                        }
+        
+                        .animation-ctn {
+                            text-align: center;
+                        }
+        
+                        @keyframes checkmark {
+                            0% {
+                                stroke-dashoffset: 100px
+                            }
+        
+                            100% {
+                                stroke-dashoffset: 0px
+                            }
+                        }
+        
+                        @keyframes checkmark-circle {
+                            0% {
+                                stroke-dashoffset: 480px
+                            }
+        
+                            100% {
+                                stroke-dashoffset: 960px
+                            }
+                        }
+        
+                        @keyframes colored-circle {
+                            0% {
+                                opacity: 0
+                            }
+        
+                            100% {
+                                opacity: 100
+                            }
+                        }
+        
+                        .icon svg {
+                            padding: 1rem;
+                        }
+        
+                        .icon svg polyline {
+                            -webkit-animation: checkmark 0.25s ease-in-out 0.7s backwards;
+                            animation: checkmark 0.25s ease-in-out 0.7s backwards
+                        }
+        
+                        .icon svg circle {
+                            -webkit-animation: checkmark-circle 0.6s ease-in-out backwards;
+                            animation: checkmark-circle 0.6s ease-in-out backwards;
+                        }
+        
+                        .icon svg circle#colored {
+                            -webkit-animation: colored-circle 0.6s ease-in-out 0.7s backwards;
+                            animation: colored-circle 0.6s ease-in-out 0.7s backwards;
+                        }
+                    </style>
+                </head>
+        
+                <body>
+                <div class="message">
+                    <div class="animation-ctn">
+                        <div class="icon">
+                            $svg
+                        </div>
+                    </div>
+        
+                    <h1>$title</h1>
+                    <p>$message</p>
                 </div>
-            </div>
-
-            <h1>$title</h1>
-            <p>$message</p>
-        </div>
-        </body>
-        </html>
-        """)
+                </body>
+                </html>
+                """
+    )
 
     def render(self, title: str, message: str, lang: str = "en", has_error: bool = False):
         return self.PAGE_TEMPLATE.substitute(
@@ -158,39 +161,57 @@ class OAuthRedirectHandler(BaseHTTPRequestHandler):
     """
     HTTPRequest Handler that is intended to be used as oauth2 callback page
     """
-    callback_template = CallbackPageTemplate()
+    _callback_template = CallbackPageTemplate()
+    LANGUAGE = "en"
+    """HTML language to render for callback page"""
 
     def log_message(self, format, *args):
+        """
+        Disables logging for BaseHTTPRequestHandler
+        """
         # silence the log messages
         pass
 
-    def do_GET(self):
+    def _serve_html(self, html: str):
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html.encode("utf-8"))
+
+    def _serve_callback_page(self, title: str, message: str, has_error: bool):
+        rendered_callback_page = self._callback_template.render(
+            lang=self.LANGUAGE,
+            title=title,
+            message=message,
+            has_error=has_error
+        )
+        self._serve_html(rendered_callback_page)
+
+    # noinspection PyPep8Naming
+    def do_GET(self) -> None:
+        """
+        Provides callback page for the oauth2 redirect
+        """
         params = parse_qs(urlparse(self.path).query)
 
-        has_error = "code" not in params or len(params['code']) != 1 or params['code'][0].strip() == ""
+        has_error = ("code" not in params
+                     or len(params['code']) != 1
+                     or params['code'][0].strip() == "")
 
         if has_error:
             self.send_response(400)
-            title = "Oh snap!"
-            message = "Something went wrong trying to authenticate you. Please try going back in your browser, or restart the auth process."
+            self._serve_callback_page(
+                title="Oh snap!",
+                message="Something went wrong trying to authenticate you. Please try going back in your browser, or restart the auth process.",
+                has_error=True,
+            )
         else:
             self.send_response(200)
             self.server._code = params["code"][0]
-            title = "Success"
-            message = "You have been authenticated successfully. You may close this browser window now and go back to the terminal"
-
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(
-            self.callback_template
-            .render(
-                lang="en",
-                title=title,
-                message=message,
-                has_error=has_error
+            self._serve_callback_page(
+                title="Success",
+                message="You have been authenticated successfully. You may close this browser window now and go back to the terminal",
+                has_error=False,
             )
-            .encode("utf-8")
-        )
 
 
 class OAuthCallbackHttpServer(HTTPServer):
@@ -201,13 +222,21 @@ class OAuthCallbackHttpServer(HTTPServer):
     def __init__(self, port):
         super().__init__(("", port), OAuthRedirectHandler)
 
-        self._code = None
+        self._code: str | None = None
 
-    def get_code(self):
+    def get_code(self) -> str | None:
+        """
+        This method should only be called after the request was done and might be None when no token is given.
+
+        :return: Authorization code or None if the request was not performed yet
+        """
         return self._code
 
     @property
-    def callback_url(self):
+    def callback_url(self) -> str:
+        """
+        Callback URL for the HTTP-Server
+        """
         return f"http://localhost:{self.server_port}"
 
     def wait_for_code(self, attempts: int = 3, timeout_per_attempt=10) -> Optional[str]:
@@ -221,7 +250,7 @@ class OAuthCallbackHttpServer(HTTPServer):
         :param timeout_per_attempt: Timeout for each attempt to be successful
         :return: Code from callback page or None if the callback page is not called successfully
         """
-        for i in range(0, attempts):
+        for _ in range(0, attempts):
             try:
                 _method_with_timeout(self.handle_request, timeout_seconds=timeout_per_attempt)
             except TimeoutException:
